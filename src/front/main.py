f515758b1628e577e282dd6c62535f18d8517b91
sensordo <sensordo@student.21-school.ru>
2023-09-16 00:27:00 +0700

    [front] lets do spa page! in 1hour night!!!! yea!

--- src/front/main.py
+++ src/front/main.py
@@ -13,12 +13,43 @@ templates = Jinja2Templates(directory="templates/")
 app.mount("/static", StaticFiles(directory="static", html=True), name="static")
 
 
-@app.get("/", response_class=HTMLResponse)
+@app.get("/login", response_class=HTMLResponse)
 def login_get(request: Request):
     context = {
         "request": request,
     }
     return templates.TemplateResponse("login.html", context)
+
+@app.get("/user", response_class=HTMLResponse)
+def login_get(request: Request):
+    context = {
+        "request": request,
+    }
+    return templates.TemplateResponse("user.html", context)
+
+@app.get("/content", response_class=HTMLResponse)
+def login_get(request: Request):
+    context = {
+        "request": request,
+    }
+    return templates.TemplateResponse("content.html", context)
+
+
+@app.get("/drop", response_class=HTMLResponse)
+def login_get(request: Request):
+    context = {
+        "request": request,
+    }
+    return templates.TemplateResponse("drop.html", context)
+
+
+
+@app.get("/admin", response_class=HTMLResponse)
+def login_get(request: Request):
+    context = {
+        "request": request,
+    }
+    return templates.TemplateResponse("admin.html", context)
 
 
 if __name__ == "__main__":
---
+++ src/front/static/js/drop.js
@@ -0,0 +1,21 @@
+function dropHandler(ev) {
+    console.log("File(s) dropped");
+    ev.preventDefault();
+    if (ev.dataTransfer.items) {
+      [...ev.dataTransfer.items].forEach((item, i) => {
+        if (item.kind === "file") {
+          const file = item.getAsFile();
+          console.log(`… file[${i}].name = ${file.name}`);
+        }
+      });
+    } else {
+      [...ev.dataTransfer.files].forEach((file, i) => {
+        console.log(`… file[${i}].name = ${file.name}`);
+      });
+    }
+  }
+
+  function dragOverHandler(ev) {
+    console.log("File(s) in drop zone");
+      ev.preventDefault();
+  }
