f5e66594f9c33842b1126df9488c6758872211a0
sensordo <sensordo@student.21-school.ru>
2023-09-17 01:14:41 +0700

    [front] get it

--- src/front/static/js/remove_containers.js
+++ src/front/static/js/remove_containers.js
@@ -3,13 +3,19 @@ function clear(){
     const find_link = document.querySelector('#find_link')
     const drop_field = document.querySelector('#drop_field')
     const search_submit_btn = document.querySelector('#search_submit_btn')
-    const canva = document.querySelector('#noisy-canvas')
-    
+    // const canva = document.querySelector('#noisy-canvas')
+
+   
+
     if (search_inp) search_inp.remove() ;
     if (find_link) find_link.remove() ;
     if (drop_field) drop_field.remove() ;
     if (search_submit_btn) search_submit_btn.remove() ;
-    if (canva) canva.remove() ;
-    
+    // if (canva) canva.remove() ;
+
+    const scriptElementDrop = document.querySelector('#script_drop');
+    if (scriptElementDrop)scriptElementDrop.remove();
+    const scriptElement = document.querySelector('#script_noise');
+    if (scriptElement)scriptElement.remove();
 }
 
